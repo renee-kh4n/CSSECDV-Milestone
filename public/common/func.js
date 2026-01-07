bbb6930d991389f850dfb88c09d34ef97fd88a3d
renee-kh4n <renee.a.f.khan@gmail.com>
2026-01-07 22:01:47 +0800

    added validation to file type

--- public/common/func.js
+++ public/common/func.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const loginForm = document.getElementById('loginForm');
+  const loginForm = document.getElementById('login-form');
   if (loginForm) {
     loginForm.addEventListener('submit', e => {
-      e.preventDefault();
-      alert('Login submitted (frontend only)');
+        e.preventDefault();
+        console.log('in func js: login')
+        alert('Login submitted (frontend only)');
     });
   }
 
-  const registerForm = document.getElementById('registerForm');
+  const registerForm = document.getElementById('register-form');
   if (registerForm) {
     registerForm.addEventListener('submit', e => {
-      e.preventDefault();
-      alert('Registration submitted (frontend only)');
+        e.preventDefault();
+        console.log('in func js: register')
+        alert('Registration submitted (frontend only)');
     });
   }
 
