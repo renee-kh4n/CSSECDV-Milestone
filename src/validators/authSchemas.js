d65efc87ecfd107a36ed3ac159a8c8ac7d06a2e3
renee-kh4n <renee.a.f.khan@gmail.com>
2026-02-13 00:22:26 +0800

    fixed 'space' bug in name (register)

--- src/validators/authSchemas.js
+++ src/validators/authSchemas.js
@@ -3,10 +3,10 @@ const { z } = require('zod');
 const registerSchema = z.object({
     firstName: z.string().trim().min(2, 'First name is too short').max(50)
         .regex(/^[A-Za-z\s'-]+$/, 'First name must only contain letters, spaces, hyphens, and apostrophes')
-        .regex(/\s{2,}/, 'First name cannot contain multiple consecutive spaces'),
+        .regex(/^(?!.*\s{2,}).*$/, "First name cannot contain multiple consecutive spaces"),
     lastName: z.string().trim().min(2, 'Last name is too short').max(50)
         .regex(/^[A-Za-z\s'-]+$/, 'Last name must only contain letters, spaces, hyphens, and apostrophes')
-        .regex(/\s{2,}/, 'Last name cannot contain multiple consecutive spaces'),
+        .regex(/^(?!.*\s{2,}).*$/, "First name cannot contain multiple consecutive spaces"),
     email: z.email('Invalid email format').trim().toLowerCase(),
     phoneNumber: z.string().trim().min(1).regex(/^9\d{9}$/, 'Enter a valid Philippine phone number'),
     password: z.string().min(8, 'Password must be at least 8 characters')
