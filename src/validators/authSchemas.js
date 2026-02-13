a87f608ba82c90d517b5b18e150ec56dba6abb72
Eem-Key <ianmatthewyanga@gmail.com>
2026-02-13 09:21:19 +0800

    phone number register upd

--- src/validators/authSchemas.js
+++ src/validators/authSchemas.js
@@ -6,9 +6,9 @@ const registerSchema = z.object({
         .regex(/^(?!.*\s{2,}).*$/, "First name cannot contain multiple consecutive spaces"),
     lastName: z.string().trim().min(2, 'Last name is too short').max(50)
         .regex(/^[A-Za-z\s'-]+$/, 'Last name must only contain letters, spaces, hyphens, and apostrophes')
-        .regex(/^(?!.*\s{2,}).*$/, "First name cannot contain multiple consecutive spaces"),
+        .regex(/^(?!.*\s{2,}).*$/, "Last name cannot contain multiple consecutive spaces"),
     email: z.email('Invalid email format').trim().toLowerCase(),
-    phoneNumber: z.string().trim().min(1).regex(/^(0\d{10}|\d{10})$/, 'Enter a valid Philippine phone number'),
+    phoneNumber: z.string().trim().min(1).regex(/^(09\d{9}|9\d{9})$/, 'Enter a valid Philippine phone number'),
     password: z.string().min(8, 'Password must be at least 8 characters')
         .regex(/[0-9]/, 'Password must contain at least one number')
         .regex(/[^a-zA-Z0-9]/, 'Password must contain at least one special character')
