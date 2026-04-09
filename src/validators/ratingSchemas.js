f9319ac37fda3d24c73960ac23d25f79ef31fb45
renee-kh4n <renee.a.f.khan@gmail.com>
2026-04-09 20:57:25 +0800

    rating with zod validator

--- src/validators/ratingSchemas.js
+++ src/validators/ratingSchemas.js
@@ -1,7 +1,7 @@
 const { z } = require('zod');
 
 const ratingSchema = z.object({
-	rating: z.number().min(1).max(5)
+    rating: z.coerce.number().min(1).max(5)
 });
 
 module.exports = { ratingSchema };
